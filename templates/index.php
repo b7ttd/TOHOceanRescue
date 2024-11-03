ce8245d066b22a1f0e1b41230f1bd8d924f38d71
b7ttd <buttdann84@gmail.com>
2024-11-03 17:51:19 -0500

    Fixed index changed things

---
+++ templates/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TOH Ocean Rescue</title>
+	<link rel = "icon" href="static/TownOfHempsteadLogo.ico" type = "images/x-icon">
+	<meta name="description" content="Ocean Rescue for Long Island, serving sine like, uh, a really long time..">
+  	<meta name="keywords" content="Lifeguards">
+  	<meta name="author" content="b7ttd">
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<link rel="stylesheet" type="text/css" href="/static/styles.css">
+	<link rel="stylesheet" type="text/css" href="w3.css"> 
+</head>
+<body id='Home'>
+<div class="myPageWrapper">
+	<div class="myPage">
+		<a name="scroll-top"></a>
+		<?php include_once("myTabs.html"); ?>
+		<div class="myPageContent">
+			<div class="myRow">
+				<div class="myCol myLeft myBlueBox">
+					<h2>Town of Hempstead Ocean Rescue</h2>
+					The Town of Hempstead's Proudest.
+					<p><a href="/templates/page1.php" class="myLinkButton">Read more...</a></p>
+				</div>
+				<div class="myCol myRight myBlueBox">
+					<h2>Ligma Balls</h2>
+					You read the message, pal. Read it and weep.
+					<p><a href="/templates/page2.php" class="myLinkButton">Read more...</a></p>
+				</div>
+			</div>
+		</div><!--endOfPageContent-->
+		<?php include_once("myFooter.html"); ?>
+	</div><!--endOfPage-->
+</div><!--endOfPageWrapper-->
+</body>
+</html>
+
